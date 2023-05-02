6c38d418866aa616c46a238c76c0fb0bc8e251ae
thifanyheerminio <111802024+thifanyheerminio@users.noreply.github.com>
2023-05-02 17:29:13 -0300

    Add files via upload

---
+++ Funcionario.php
@@ -0,0 +1,23 @@
+<?php
+
+    class Funcionario{
+        public $nome
+        public $cpf
+        public $telefone
+        public $endreço
+        public $função
+        public $salario
+        public $email
+        public $turno
+
+        public function mostrarDados(){
+          
+
+        }
+       public function calculaDesconto(desconto){
+         $this->salario = $this->salario - 
+       }
+    }
+
+?>
+
